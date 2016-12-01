6b8251a0c647f46f23f333a5c61e6df57e2d6752
Eric Chan <The-Chanman@users.noreply.github.com>
2016-11-30 21:10:58 -0800

    Update sensor.js
    Better syntax with promises in preparetopublish chain

--- sensor.js
+++ sensor.js
@@ -8,7 +8,6 @@ const frequency = 60 * 60 * 1000
 
 // parse into url object 
 let base = 'http://erddap.exploratorium.edu:8080/erddap/tabledap/explorebeaconbay5min.json?time,Temperature_1,Air_Pressure,Temperature_2,Relative_Humidity,Dew_Point,O3_1,CO_1,NO2_1,O3_Working_2,O3_Auxiliary_2,CO_Working_2,CO_Auxiliary_2,NO_Working,NO_Auxiliary,NO_Auxiliary_2,NO2_Working_2,NO2_Auxiliary_2,Particulate_high,Particulate_total,Particulate_pct,CO2,Temperature_3,station_id,latitude,longitude&time>='
-//'http://erddap.exploratorium.edu:8080/erddap/tabledap/exploreusgsdata.json?time,temperature,specific_conductance,salinity,turbidity,dissolved_o2,station_id,latitude,longitude&time>='
 
 function buildUrl() {
   const d = new moment()
@@ -48,16 +47,12 @@ nomad.prepareToPublish().then((n) => {
   return instance.publishRoot('hello')
 }).then(() => {
   setInterval(() => {
-    getMessage().then(m => {
-      instance.publish(m)
-      .catch(err => {
-        console.log(`Error: ${err}`)
-      })
-    })
+    getMessage().then(m => return instance.publish(m))
     .catch(err => {
       console.log(`Error: ${err}`)
     })
   }, frequency)  
 })
+ .catch(console.log)
 
 
