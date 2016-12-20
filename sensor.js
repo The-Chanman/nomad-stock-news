4c53c6708e11fcf37afcd728b207af8272897bc1
The-Chanman <ecphylo@gmail.com>
2016-12-19 20:20:18 -0800

    got this working with a pretty heavy data parser

--- sensor.js
+++ sensor.js
@@ -3,15 +3,23 @@ const moment = require('moment')
 const nomad = new Nomad()
 const fetch = require('node-fetch')
 
+let keep = {
+  link: 1,
+  pubDate: 1,
+  title: 1
+}
+let results = {}
 let instance = null
 const frequency = 30 * 60 * 1000
 
 // parse into url object 
-const stocks = '("AAPL", "YHOO", "GOOGL")'
-let url = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20${encodeURI(stocks)}&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=`
+const searchQuery = ['apple', 'yahoo', 'google']
+let url = [`https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnews.google.com%2Fnews%2Fsection%3Fq%3D${searchQuery[0]}%26output%3DRSS`, 
+`https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnews.google.com%2Fnews%2Fsection%3Fq%3D${searchQuery[1]}%26output%3DRSS`,
+`https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnews.google.com%2Fnews%2Fsection%3Fq%3D${searchQuery[2]}%26output%3DRSS`]
 
-function getMessage() {
-  return fetch(url)
+function getMessage(index) {
+  return fetch(url[index])
     .then(res => res.json())
     .catch(err => {
       console.log('getMessage error: ', err)
@@ -21,15 +29,43 @@ function getMessage() {
 
 function startPoll(frequency) {
   setInterval(() => {
-    getMessage()
+    getMessage(0)
       .then((m) => {
-        console.log('fetched:', JSON.stringify(m))
-        let results = {
-          time: m.query.created,
-          lang: m.query.lang,
-          results: m.query.results.quote
+        // console.log('fetched:', JSON.stringify(m))
+        setTimeout(() => {
+          results[searchQuery[0]] = m
+        }, 10000)
+        return getMessage(1)
+      })
+      .then((m) => {
+        // console.log('fetched:', JSON.stringify(m))
+        setTimeout(() => {
+          results[searchQuery[1]] = m
+        }, 10000)
+        return getMessage(2)
+      })
+      .then((m) => {
+        // console.log('fetched:', JSON.stringify(m))
+        setTimeout(() => {
+          results[searchQuery[2]] = m
+        }, 10000)
+
+      for (var key in results) {
+        if (!results.hasOwnProperty(key)) { continue }
+        for (var prop in results[key]["items"]){
+          for (var value in results[key]["items"][prop]) {
+            if (!keep[value]){
+              delete results[key]["items"][prop][value]
+            }
+            if (value == "link"){
+              let urlIndex = results[key]["items"][prop]["link"].indexOf("url=")
+              results[key]["items"][prop]["link"] = results[key]["items"][prop]["link"].substring(urlIndex+4)
+            }
+          }
         }
-        return instance.publish(JSON.stringify(results))
+      }
+      console.log(JSON.stringify(results))
+      return instance.publish(JSON.stringify(results))
       })
       .catch(console.log)
   }, frequency)
@@ -38,7 +74,7 @@ function startPoll(frequency) {
 nomad.prepareToPublish()
   .then((node) => {
     instance = node
-    return instance.publishRoot('hello, get ready for stock quotes!')
+    return instance.publishRoot('hello, get ready for stock news!')
   })
   .then(() => startPoll(frequency))
   .catch(console.log)
