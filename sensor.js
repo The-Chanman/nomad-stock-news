72a91cc734909997b97a8477bbb1482a894f8475
The-Chanman <ecphylo@gmail.com>
2016-12-19 17:49:59 -0800

    lets get that stocks stuff

--- sensor.js
+++ sensor.js
@@ -4,60 +4,41 @@ const nomad = new Nomad()
 const fetch = require('node-fetch')
 
 let instance = null
-const frequency = 5 * 60 * 1000
+const frequency = 30 * 60 * 1000
 
 // parse into url object 
-let base = 'http://erddap.exploratorium.edu:8080/erddap/tabledap/explorebeaconbay5min.json?time,Temperature_1,Air_Pressure,Temperature_2,Relative_Humidity,Dew_Point,O3_1,CO_1,NO2_1,O3_Working_2,O3_Auxiliary_2,CO_Working_2,CO_Auxiliary_2,NO_Working,NO_Auxiliary,NO_Auxiliary_2,NO2_Working_2,NO2_Auxiliary_2,Particulate_high,Particulate_total,Particulate_pct,CO2,Temperature_3,station_id,latitude,longitude&time>='
+const stocks = '("AAPL", "YHOO", "GOOGL")'
+let url = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20${encodeURI(stocks)}&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=`
 
-function buildUrl() {
-  const d = new moment()
-  const twohourAgo = d.subtract(2, 'h')
-  const timeString = twohourAgo.toISOString()
-  const url = `${base}${timeString}`
-  const encoded = encodeURI(url)
-  return encoded
-}
-
-function transform(json) {
-  let transformed = {}
-  transformed['columnNames'] = json.table.columnNames
-  transformed['columnTypes'] = json.table.columnTypes
-  transformed['columnUnits'] = json.table.columnUnits
-  transformed['data'] = json.table.rows[(json.table.rows.length-1)]
-  return transformed
-}
-
-// returns promise
 function getMessage() {
-  const url = buildUrl()
-  return fetch(url).then(res => {
-  	console.log(res.body)
-    return res.json();
-  }).then(json => {
-    const formatted = transform(json)
-    return Promise.resolve(JSON.stringify(formatted))
-  }).catch(err => {
-    console.log(err)
-    return Promise.reject(err)
-  })
+  return fetch(url)
+    .then(res => res.json())
+    .catch(err => {
+      console.log('getMessage error: ', err)
+      return err
+    })
 }
 
-nomad.prepareToPublish().then((n) => {
-  instance = n
-  return instance.publishRoot('hello')
-}).then(() => {
+function startPoll(frequency) {
   setInterval(() => {
-    getMessage().then(m => {
-      instance.publish(m)
-      .catch(err => {
-        console.log(`Error: ${err}`)
+    getMessage()
+      .then((m) => {
+        console.log('fetched:', JSON.stringify(m))
+        let results = {
+          time: m.query.created,
+          lang: m.query.lang,
+          results: m.query.results.quote
+        }
+        return instance.publish(JSON.stringify(results))
       })
-    })
-    .catch(err => {
-      console.log(`Error: ${err}`)
-    })
-  }, frequency)  
-})
- .catch(console.log)
+      .catch(console.log)
+  }, frequency)
+}
 
-
+nomad.prepareToPublish()
+  .then((node) => {
+    instance = node
+    return instance.publishRoot('hello, get ready for stock quotes!')
+  })
+  .then(() => startPoll(frequency))
+  .catch(console.log)
